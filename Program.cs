887d07f9cba3fa42d05c6792722e3e8f11564e07
Benett Viszokai <145262915+benettviszokai@users.noreply.github.com>
2023-09-17 14:11:51 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This is an ASCII art program - creating a creature purely with ASCII characters.
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| o |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("This is a Ghost. Spooky.");
+
+      // Optional task: creating the Dancing Man
+      Console.WriteLine("c_     ___ ");
+      Console.WriteLine("　 \\\\__|__|__");
+      Console.WriteLine("　　 \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");
+      Console.WriteLine("　|　|  \\");
+      Console.WriteLine("　| / \\  )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+     }
+  }
+}
